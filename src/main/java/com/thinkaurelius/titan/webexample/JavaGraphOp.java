ecd0c8ad5282114b59df518d20f625502e47b45f
李阿兰 <AlanLi@alan-2.local>
2016-11-25 23:15:38 -0500

    Add another version of finding shortest path and find friends recommend

--- src/main/java/com/thinkaurelius/titan/webexample/JavaGraphOp.java
+++ src/main/java/com/thinkaurelius/titan/webexample/JavaGraphOp.java
@@ -142,21 +142,42 @@ public class JavaGraphOp {
 
     /**
      * Query: Do I share any common interests with someone I wish to know?
-     * @param targetPerson name of the person I wish to know
+     * @param targetPerson Name of the person I wish to know
      * @return List of interests that we share together
+     * TODO: what is the format of the input for the interests? List of Strings? Or just strings
      */
-    public List<String> findCommonInterestsWithSomeone(String me, String targetPerson) {
+    public List<String> findCommonInterestsWithSomeone(String myName, String allMyInterests, String targetPerson) {
         if (targetPerson == null || targetPerson.length() == 0) {
             return "";
         }
 
         /* Check if this people is in our system */
+        Vertex targetVertex = graph.getVertex("name", targetPerson).next();
+        if (targetVertex == null) {
+            throw new RequiredVertexNotFoundException("Expect to find user:" + targetPerson + " in our system but does not exist.");
+        }
 
+        /* Use a set to find the common interests we share */
+        String[] myInterests = allMyInterests.split(", ");
+        Set<String> myInterestSet = new HashSet<>();
+        for (string item : myInterests) {
+            myInterestSet.add(item);
+        }
+
+        List<String> result = new ArrayList<>();
+        for (Vertex currentVertex : targetVertex.outE(LIKES).inV().toList()) {
+            String thisInterest = currentVertex.property("name").value();
+            if (myInterestSet.contains(thisInterest)) {
+                result.add(thisInterest);
+            }
+        }
+        return result;
     }
 
     /**
-     * Query: How many people am I away from the person I wish I knew?
+     * Query: How many people am I away from the person I wish I knew? (The shortest path between two nodes)
      * @return The list of name of people that connects me to the person, if there is any
+     * TODO : make sure which of the version of finding the shortest path will work
      */
     public String getDegreeFromTargetPerson(String startPeople, String targetPeople) {
         List<String> path = new ArrayList<>();
@@ -164,8 +185,10 @@ public class JavaGraphOp {
         final Vertex vA = graph.traversal().V().has("name", startPeople).next();
         final Vertex vB = graph.traversal().V().has("name", targetPeople).next();
 
-        final GremlinPipeline pipe = new GremlinPipeline(vA).as("person")
-                .both("iHas","studyIn","hHas","interestIn").loop("person", new PipeFunction<LoopPipe.LoopBundle, Boolean>() {
+        final GremlinPipeline pipe = new GremlinPipeline(vA)
+                .as("person")
+                .both(HAS_PEOPLE_INTERESTEDIN, ATTENDS, LIKES) /* TODO : Understand what's happening here */
+                .loop("person", new PipeFunction<LoopPipe.LoopBundle, Boolean>() {
                     @Override
                     public Boolean compute(LoopPipe.LoopBundle loopBundle) {
                         return loopBundle.getLoops() < MAX_LOOP_STEPS && loopBundle.getObject() != vB;
@@ -180,6 +203,91 @@ public class JavaGraphOp {
         }
         return path.size() == 0 ? "Can't connect " + startPeople + " to " + targetPeople : path.toString();
     }
+
+    /**
+     * TODO : is this version working?
+     * Another version of finding the shortest path between two user in the system.
+     */
+    public List<String> getShortestPathVersion2(String startPerson, String endPerson) {
+        if (startPerson == null || startPerson.length() == 0 || endPerson == null || endPerson.length() == 0) {
+            return "";
+        }
+
+        GraphTraversalSource traversal = graph.traversal();
+        Vertex fromNode = g.V().has("name", startPerson).next();
+        Vertex toNode = g.V().has("name", endPerson).next();
+        if (fromNode == null || toNode == null) {
+            throw new RequiredVertexNotFoundException("Expect both users in our system but somehow at least one of them is missing.");
+        }
+
+        List<String> result = new ArrayList();
+        g.V(fromNode).repeat(both().simplePath())
+                .until(is(toNode))
+                .limit(1) /* Only output 1 path for all possible results we have here */
+                .path().fill(result); /* TODO: Simple path???? */
+
+        return result;
+    }
+
+
+    /**
+     * Query: Recommend people you may want to meet (recommend friends' friend)
+     * @return List of people that recommend me to know. They should be in decending order
+     *         by the number of people we both know.
+     *         We at most return 5 people to the user.
+     * TODO: the input format for the friends.
+     * TODO: Decide the format for the output. (List? String? Map?)
+     */
+    public Map<String, Integer> getFriendsRecommend(String myName, String friendsNames) {
+        if (myName == null || myName.length() == 0 || friendsNames == null || friendsNames.length() == 0) {
+            return "";
+        }
+
+        /* Find all friends for my friends, and find the number of people we know in common */
+        String[] friends = friendsNames.split(", ");
+        Set<String> myFriends = new HashSet<>();
+        for (String person : friends) {
+            myFriends.add(person);
+        }
+
+        List<Pair> peopleRecommend = new ArrayList<>();
+        for (String curPerson : friends) {
+            Vertex curVertex = graph.getVertex("name", curPerson).next();
+            if (curVertex == null) {
+                continue;
+            }
+            // All friends for my friend
+            List<Vertex> friendsOfMyFriend = curVertex.outE(KNOWS).inV().toList();
+
+            // Let's see if I know any of his friends.
+            int numOfCommonFriends = 0;
+            for (Vertex person : friendsOfMyFriend) {
+                for (Vertex current : person.outE(KNOWS).inV().toList()) {
+                    if (myFriends.contains(current.property("name").value())) {
+                        numOfCommonFriends++;
+                    }
+                }
+
+                if (numOfCommonFriends != 0) {
+                    peopleRecommend.add(new Pair(person.property("name").value(), numOfCommonFriends));
+                }
+            }
+        }
+
+        // Sort them in the decending order
+        Arrays.sort(peopleRecommend, (p1, p2) -> p2.number - p1.number);
+        Map<String, Integer> result = new HashMap<>();
+        for (int i = 0; i < peopleRecommend.size; i++) {
+            if (i == 5) {// Limit the number of recommendation to be 5 at max
+                break;
+            }
+
+            result.put(peopleRecommend.get(i).name, peopleRecommend.get(i).number);
+        }
+
+        return result;
+    }
+
 
     public String getRecommend(String person) {
         String res = getPersonByEdge("name", "UF", "studyIn");
@@ -223,6 +331,19 @@ public class JavaGraphOp {
             res++;
         }
         return Long.toString(res);
+    }
+
+    /**
+     * A wrapper class created for the friends recommendation query
+     */
+    private class Pair {
+        String name;
+        int number;
+
+        public Pair(String name, int num) {
+            this.name = name;
+            this.number = num;
+        }
     }
 
     /**
