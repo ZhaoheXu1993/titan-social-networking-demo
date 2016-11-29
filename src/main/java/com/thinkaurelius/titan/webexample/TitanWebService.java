11fc2925bfb32443474584fcdb5eb978da5201b9
Joe Xu <zhaohexu1993@gmail.com>
2016-11-29 00:51:24 -0500

    v1

--- src/main/java/com/thinkaurelius/titan/webexample/TitanWebService.java
+++ src/main/java/com/thinkaurelius/titan/webexample/TitanWebService.java
@@ -1,17 +1,19 @@
 package com.thinkaurelius.titan.webexample;
 
+import org.codehaus.jettison.json.JSONArray;
 import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
+import org.elasticsearch.search.aggregations.InternalAggregation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.PostConstruct;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.UriInfo;
-import java.util.Map;
+import java.util.*;
 
 /**
  * This class handles the HTTP requests.
@@ -19,7 +21,6 @@ import java.util.Map;
 @Path("/")
 @Component
 public class TitanWebService {
-
     @Autowired
     GroovyGraphOp groovyOp;
 
@@ -32,84 +33,196 @@ public class TitanWebService {
     }
 
     @GET
-    @Path("/friendRecommend")
-    @Produces(MediaType.TEXT_PLAIN)
-    public String getRecommand(@Context UriInfo info) throws JSONException {
-        String myName = "UF--student--1";
-        String friendsName = "UF--student--2, UF--student--3, UF--student--4, UF--student--5" +
-                ", UF--student--6, UF--student--7, UF--student--7, UF--student--9" +
-                ", UF--student--10, UF--student--11, UF--student--12, UF--student--13";
-        Map<String, Integer> mapRes = operationHandeler.getFriendsRecommend(myName, friendsName);
-        String res = "";
-        for (Map.Entry entry : mapRes.entrySet()) {
-            res += entry.getKey() + ", " + entry.getValue() + "\n";
+    @Path("/school/{schoolName}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public @ResponseBody String getStudentBySchool(@PathParam("schoolName") String schoolName) throws JSONException{
+        Map<String, String> nameMajorMap = operationHandeler.getPeopleInTheSameSchool(schoolName);
+        JSONArray res = new JSONArray();
+        for (Map.Entry entry : nameMajorMap.entrySet()) {
+            JSONObject person = new JSONObject();
+            person.put("name", entry.getKey());
+            person.put("major", entry.getValue());
+            res.put(person);
         }
-        return "\"" + res + "\"";
+        return res.toString();
+    }
+
+    @GET
+    @Path("/major/{schoolName}/{majorName}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public @ResponseBody String getStudentByMajorAndSchool(@PathParam("schoolName") String schoolName,
+                                                           @PathParam("majorName") String majorName) throws JSONException {
+        Map<String, String> nameInterestMap = operationHandeler.getPeopleWithSameMajor(majorName, schoolName);
+        JSONArray res = new JSONArray();
+        for (Map.Entry entry : nameInterestMap.entrySet()) {
+            JSONObject person = new JSONObject();
+            person.put("name", entry.getKey());
+            person.put("interest", entry.getValue());
+            res.put(person);
+        }
+        return res.toString();
+    }
+
+    @GET
+    @Path("/interest/{schoolName}/{interestsNames}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public @ResponseBody String getStudentByInterestAndSchool(@PathParam("schoolName") String schoolName,
+                                                              @PathParam("interestsNames") String interestsNames) throws JSONException {
+        Map<String, String> nameMajorMap = operationHandeler.getPeopleWithSameInterestsInSchool(interestsNames, schoolName);
+        JSONArray res = new JSONArray();
+        for (Map.Entry entry : nameMajorMap.entrySet()) {
+            JSONObject person = new JSONObject();
+            person.put("name", entry.getKey());
+            person.put("major", entry.getValue());
+            res.put(person);
+        }
+        return res.toString();
+    }
+
+    @GET
+    @Path("/commonInterest/{sourceStudentName}/{targetStudentName}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public @ResponseBody String getCommonInterestWith(@PathParam("sourceStudentName") String sourceName,
+                                                      @PathParam("targetStudentName") String targetName) throws JSONException {
+        List<String> commonInterestList = operationHandeler.findCommonInterestsWithSomeone(sourceName, targetName);
+
+        JSONObject res = new JSONObject();
+        res.put("name", new ArrayList<String>(commonInterestList));
+
+        return res.toString();
+    }
+
+    @GET
+    @Path("/findDegree/{FromStudentName}/{ToStudentName}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public @ResponseBody String getDegree(@PathParam("FromStudentName") String fromName,
+                                          @PathParam("ToStudentName") String toName) {
+        List<String> res = operationHandeler.getShortestPathVersion2(fromName, toName);
+
+        return res.toString();
     }
 
     @GET
     @Path("/getVerticesCount")
-    @Produces(MediaType.TEXT_PLAIN)
-    public String getVerticesCount(@Context UriInfo info) throws JSONException {
-        String res = operationHandeler.getVerticesCount();
-        return "\"" + res + "\"";
+    @Produces(MediaType.APPLICATION_JSON)
+    public @ResponseBody String getVerticesCount(@Context UriInfo info) throws JSONException {
+        JSONObject res = new JSONObject();
+        String num = operationHandeler.getVerticesCount();
+        res.put("count", num);
+        return res.toString();
     }
 
     @GET
-    @Path("/school/uf")
-    @Produces(MediaType.TEXT_PLAIN)
-    public String getPersonBySchoolName(@Context UriInfo info) throws JSONException {
-        String res = operationHandeler.getPersonByEdge("name", "UF", "studyIn");
-        return "\"" + res + "\"";
+    @Path("/friendRecommend/{name}/{friendNames}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public @ResponseBody
+    String getRecommand(@Context UriInfo info,
+                                  @PathParam("name") String studentName,
+                                  @PathParam("friendNames") String friendNames) throws JSONException {
+        String friendsNames = friendNames.replace("_",", ");
+        Map<String, Integer> mapRes = operationHandeler.getFriendsRecommend(studentName, friendsNames);
+
+        JSONArray res = new JSONArray();
+
+        for (Map.Entry entry : mapRes.entrySet()) {
+            JSONObject tmp = new JSONObject();
+            tmp.put("name", entry.getKey());
+            tmp.put("common_num", entry.getValue());
+            res.put(tmp);
+        }
+
+        return res.toString();
     }
 
-    @GET
-    @Path("/interest/football")
-    @Produces(MediaType.TEXT_PLAIN)
-    public String getPersonByInterestName(@Context UriInfo info) throws JSONException {
-        String res = operationHandeler.getPersonByEdge("name", "football", "interestIn");
-        return "\"" + res + "\"";
-    }
+    @POST
+    @Path("/addStudent/{studentName}/{schoolName}/{interestNames}/{majorName}/{friendNames}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public @ResponseBody String addStudent(@PathParam("studentName") String studentName,
+                                           @PathParam("schoolName") String schoolName,
+                                           @PathParam("interestNames") String interestName,
+                                           @PathParam("majorName") String majorName,
+                                           @PathParam("friendNames") String friendNames) throws JSONException {
+//        List<String> notFoundName = operationHandeler.addStudent(
+//                studentName,
+//                schoolName,
+//                interestName,
+//                majorName,
+//                friendNames
+//                );
+//        JSONArray results = new JSONArray();
+//
+//        for (String name : notFoundName) {
+//            JSONObject person = new JSONObject();
+//            person.put("not_found", name);
+//            results.put(person);
+//        }
 
+        return "";
+    }
 
 //    @GET
-//    @Path("/search/JoeToKobe")
+//    @Path("/getVerticesCount")
 //    @Produces(MediaType.TEXT_PLAIN)
-//    public String shortestPath(@Context UriInfo info) throws JSONException {
-//        String res = operationHandeler.getDegree("Joe", "Kobe");
+//    public String getVerticesCount(@Context UriInfo info) throws JSONException {
+//        String res = operationHandeler.getVerticesCount();
 //        return "\"" + res + "\"";
 //    }
-
-    @GET
-    @Path("/recommend/Joe")
-    @Produces(MediaType.TEXT_PLAIN)
-    public String getRecommend(@Context UriInfo info) throws JSONException {
-        String res = operationHandeler.getRecommend("Joe");
-        return "\"" + res + "\"";
-    }
-
-    /*
-    @GET
-    @Path("/rank/Joe")
-    @Produces(MediaType.TEXT_PLAIN)
-    public String listVertices(@Context UriInfo info) throws JSONException {
-        String res = operationHandeler.listVertices();
-        return "\"" + res + "\"";
-    }
-    */
-    @GET
-    @Path("/listVertices/name")
-    @Produces(MediaType.TEXT_PLAIN)
-    public String listVertices(@Context UriInfo info) throws JSONException {
-        String res = operationHandeler.listVertices();
-        return "\"" + res + "\"";
-    }
-
-    @GET
-    @Path("/plutosBrothers")
-    @Produces(MediaType.TEXT_PLAIN)
-    public String pipeline(@Context UriInfo info) throws JSONException {
-        String res = groovyOp.getPlutosBrothers();
-        return "\"" + res + "\"";
-    }
+//
+//    @GET
+//    @Path("/school/uf")
+//    @Produces(MediaType.TEXT_PLAIN)
+//    public String getPersonBySchoolName(@Context UriInfo info) throws JSONException {
+//        String res = operationHandeler.getPersonByEdge("name", "UF", "studyIn");
+//        return "\"" + res + "\"";
+//    }
+//
+//    @GET
+//    @Path("/interest/football")
+//    @Produces(MediaType.TEXT_PLAIN)
+//    public String getPersonByInterestName(@Context UriInfo info) throws JSONException {
+//        String res = operationHandeler.getPersonByEdge("name", "football", "interestIn");
+//        return "\"" + res + "\"";
+//    }
+//
+//
+////    @GET
+////    @Path("/search/JoeToKobe")
+////    @Produces(MediaType.TEXT_PLAIN)
+////    public String shortestPath(@Context UriInfo info) throws JSONException {
+////        String res = operationHandeler.getDegree("Joe", "Kobe");
+////        return "\"" + res + "\"";
+////    }
+//
+//    @GET
+//    @Path("/recommend/Joe")
+//    @Produces(MediaType.TEXT_PLAIN)
+//    public String getRecommend(@Context UriInfo info) throws JSONException {
+//        String res = operationHandeler.getRecommend("Joe");
+//        return "\"" + res + "\"";
+//    }
+//
+//    /*
+//    @GET
+//    @Path("/rank/Joe")
+//    @Produces(MediaType.TEXT_PLAIN)
+//    public String listVertices(@Context UriInfo info) throws JSONException {
+//        String res = operationHandeler.listVertices();
+//        return "\"" + res + "\"";
+//    }
+//    */
+//    @GET
+//    @Path("/listVertices/name")
+//    @Produces(MediaType.TEXT_PLAIN)
+//    public String listVertices(@Context UriInfo info) throws JSONException {
+//        String res = operationHandeler.listVertices();
+//        return "\"" + res + "\"";
+//    }
+//
+//    @GET
+//    @Path("/plutosBrothers")
+//    @Produces(MediaType.TEXT_PLAIN)
+//    public String pipeline(@Context UriInfo info) throws JSONException {
+//        String res = groovyOp.getPlutosBrothers();
+//        return "\"" + res + "\"";
+//    }
 }
